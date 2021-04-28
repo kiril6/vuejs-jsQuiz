c4e25df886f365d14e647320f214275a088a13ce
Kiril Delovski <kiril.delovski@swisscom.com>
2021-04-28 14:51:24 +0200

    configuration files update

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? 'https://js-quiz.delovski.net/' : '/',
+    publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
 };
